ef890b16efe8a415d3e48b6931cea1741e6c2255
yuzu <yurkazubkov@yandex.ru>
2024-07-15 22:29:00 +0300

    Update FishDraw

--- src/fish.c
+++ src/fish.c
@@ -5,8 +5,17 @@
 
 #include <raylib.h>
 #include <raymath.h>
+#include <rlgl.h>
 
-static const float radiuses[FISH_POINTS_COUNT] = {20, 25, 25, 25, 22, 20, 15, 10, 9, 8}; 
+static const float radiuses[FISH_POINTS_COUNT] = {20, 25, 25, 25, 22, 20, 15, 12, 9, 8}; 
+
+static float GetAngle(Fish *fish, size_t pointIndex) {
+    return Vector2Angle((Vector2){1, 0}, Vector2Normalize(Vector2Subtract(fish->chain.points[pointIndex + 1], fish->chain.points[pointIndex])));
+}
+
+static Vector2 GetPos(Fish *fish, size_t i, float angleOffset, float radiusOffset) {
+    return Vector2Add(fish->chain.points[i], Vector2Rotate((Vector2){radiuses[i] + radiusOffset}, GetAngle(fish, i) + angleOffset));
+}
 
 void FishInit(Fish *fish, Vector2 points[FISH_POINTS_COUNT]) {
     fish->chain.points = points;
@@ -22,12 +31,40 @@ void FishResolve(Fish *fish, Vector2 targetPos) {
 }
 
 void FishDraw(Fish *fish) {
+    size_t ind = 2;
+    Vector2 v = GetPos(fish, ind, PI/2, 0);
+    rlPushMatrix();
+    rlTranslatef(v.x, v.y, 0);
+    rlRotatef(RAD2DEG*GetAngle(fish, ind)+60, 0, 0, 1);
+    DrawEllipse(0, 0, 16, 8, Fade(BLUE, 0.8));
+    rlPopMatrix();
+    v = GetPos(fish, ind, -PI/2, 0);
+    rlPushMatrix();
+    rlTranslatef(v.x, v.y, 0);
+    rlRotatef(RAD2DEG*GetAngle(fish, ind)-60, 0, 0, 1);
+    DrawEllipse(0, 0, 16, 8, Fade(BLUE, 0.8));
+    rlPopMatrix();
+    
+    ind = 8;
+    v = GetPos(fish, ind, PI/2, 0);
+    rlPushMatrix();
+    rlTranslatef(v.x, v.y, 0);
+    rlRotatef(RAD2DEG*GetAngle(fish, ind)+60, 0, 0, 1);
+    DrawEllipse(0, 0, 16, 8, Fade(BLUE, 0.8));
+    rlPopMatrix();
+    v = GetPos(fish, ind, -PI/2, 0);
+    rlPushMatrix();
+    rlTranslatef(v.x, v.y, 0);
+    rlRotatef(RAD2DEG*GetAngle(fish, ind)-60, 0, 0, 1);
+    DrawEllipse(0, 0, 16, 8, Fade(BLUE, 0.8));
+    rlPopMatrix();
+
     for (size_t i = 0; i < FISH_POINTS_COUNT; ++i) {
         DrawCircleV(fish->chain.points[i], radiuses[i], BLUE);
     }
 
     for (size_t i = 1; i < FISH_POINTS_COUNT; ++i) {
-        float a = Vector2Angle((Vector2){1, 0}, Vector2Normalize(Vector2Subtract(fish->chain.points[i], fish->chain.points[i - 1])));
+        float a = GetAngle(fish, i - 1);
         // https://en.wikipedia.org/wiki/Belt_problem#Pulley_problem
         float angle = acosf((radiuses[i - 1] - radiuses[i]) / Vector2Distance(fish->chain.points[i - 1], fish->chain.points[i]));
 
@@ -39,4 +76,13 @@ void FishDraw(Fish *fish) {
         DrawTriangle(v11, v21, v12, BLUE);
         DrawTriangle(v21, v22, v12, BLUE);
     }
+
+    ind = 0;
+    v = GetPos(fish, ind, PI/3, 0);
+    DrawCircleV(v, 5, RAYWHITE);
+    DrawCircleV(v, 3, BLACK);
+    v = GetPos(fish, ind, -PI/3, 0);
+    DrawCircleV(v, 5, RAYWHITE);
+    DrawCircleV(v, 3, BLACK);
 }
+
--- src/main.c
+++ src/main.c
@@ -9,29 +9,23 @@ int main(void) {
 
     SetTargetFPS(60);
     
-    Fish fish1;
-    FishInit(&fish1, (Vector2[FISH_POINTS_COUNT]){0});
-    Fish fish2;
-    FishInit(&fish2, (Vector2[FISH_POINTS_COUNT]){0});
+    Fish fish;
+    FishInit(&fish, (Vector2[FISH_POINTS_COUNT]){0});
 
     Vector2 mousePos = GetMousePosition();
-    Vector2 targetPos1 = mousePos;
-    Vector2 targetPos2 = mousePos;
+    Vector2 targetPos = mousePos;
     
     while (!WindowShouldClose()) {
         // Update
         mousePos = GetMousePosition();
         
-        targetPos1 = Vector2MoveTowards(targetPos1, mousePos, 5);
-        FishResolve(&fish1, targetPos1);
-        targetPos2 = Vector2MoveTowards(targetPos2, targetPos1, 2.5);
-        FishResolve(&fish2, targetPos2);
+        targetPos = Vector2MoveTowards(targetPos, mousePos, 5);
+        FishResolve(&fish, targetPos);
 
         // Draw
         BeginDrawing();
             ClearBackground(BLACK);
-            FishDraw(&fish1);
-            FishDraw(&fish2);
+            FishDraw(&fish);
         EndDrawing();
     }
 }
